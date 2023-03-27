2a1342ffbbe8b09f85a28fbb91f549a3ab3cfa81
Vivek Jilla <vivekjilla7@gmail.com>
2023-03-27 11:46:53 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 module.exports = {
+  output: 'standalone',
   eslint: {
     dirs: ['src'],
   },
+  distDir: 'outputFolder',
   experimental: {
     // Defaults to 50MB
     isrMemoryCacheSize: 0,
